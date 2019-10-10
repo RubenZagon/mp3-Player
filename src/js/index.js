99f7d3fb9dc1494cae47e2a3438f6530300167ad
Ruben Zagon <ruben.zagon@gmail.com>
2019-10-10 17:05:29 +0100

    Fixit a bug :bug:

--- src/js/index.js
+++ src/js/index.js
@@ -3,11 +3,11 @@ const songs = [
     'https://scummbar.com/mi2/MI1-CD/02%20-%20Chapter%20Screen.mp3',
     'https://scummbar.com/mi2/MI1-CD/03%20-%20The%20Scumm%20Bar.mp3'
 ];
-​
+
 let currentSong = 0;
 let audio = new Audio(songs[currentSong]); // Descarga la primera canción
 audio.volume = 0.5;
-​
+
 const divText = document.querySelector('.text');
 const buttons = {
     play: document.querySelector('.play'),
@@ -17,55 +17,54 @@ const buttons = {
     stop: document.querySelector('.stop'),
     volume: document.querySelector('.volume')
 }
-​
+
 function fnPlay() {
     prepareSong();
     audio.play();
     putText();
 }
-​
+
 function prepareSong() {
     fnPause();
     audio = new Audio(songs[currentSong]);
     audio.volume = 0.5;
 }
-​
+
 function fnPause() {
     audio.pause();
 }
-​
+
 function fnPrev() {
     currentSong--;
-​
+
     if (currentSong < 0) {
         currentSong = songs.length - 1;
     }
-​
+
     fnPlay();
 }
-​
+
 function fnNext() {
     currentSong++;
-​
     if (currentSong >= songs.length) {
         currentSong = 0;
     }
-​
+
     fnPlay();
 }
-​
+
 function fnStop() {
     audio.currentTime = 0;
     fnPause();
 }
-​
+
 function changeVolume(vol, name) {
     audio.volume = vol;
     buttons.volume.innerHTML = `<i class="fas fa-volume-${name}"></i>`;
 }
-​
+
 function fnVolume() {
-​
+
     // Way 1 (ERROR!!)
     // if (audio.volume == 0.5) {
     //     audio.volume = 1;
@@ -79,7 +78,7 @@ function fnVolume() {
     //     audio.volume = 0.5;
     //     buttons.volume.innerHTML = '<i class="fas fa-volume-down"></i>';
     // }
-​
+
     // Way 2 (ELSE!)
     // if (audio.volume == 0.5) {
     //     audio.volume = 1;
@@ -91,7 +90,7 @@ function fnVolume() {
     //     audio.volume = 0.5;
     //     buttons.volume.innerHTML = '<i class="fas fa-volume-down"></i>';
     // }
-​
+
     // Way 3 (Refactor)
     // En nuestro caso, se puede eliminar el último if
     // if (audio.volume == 0.5)
@@ -100,7 +99,7 @@ function fnVolume() {
     //     changeVolume(0, 'mute');
     // else if (audio.volume == 0)
     //     changeVolume(0.5, 'down');
-​
+
     // Way 4 (Switch)
     // switch (audio.volume) {
     //     case 0.5:
@@ -116,7 +115,7 @@ function fnVolume() {
     //         alert('ERROR: Esto no puede ocurrir');
     //         break;
     // }
-​
+
     // Way 5 (CRAZY JAVASCRIPT ULTIMATE REFACTOR)
     const cases = {
         0.5: changeVolume.bind(null, 1, 'up'),
@@ -124,9 +123,9 @@ function fnVolume() {
         0: changeVolume.bind(null, 0.5, 'down')
     }
     cases[audio.volume]();
-​
+
 }
-​
+
 function putText() {
     const nameSong = songs[currentSong];
     const cleanName = unescape(nameSong);
@@ -135,7 +134,7 @@ function putText() {
     const miniName = shortName.replace('.mp3', '');
     divText.innerHTML = miniName;
 }
-​
+
 buttons.play.addEventListener('click', fnPlay);
 buttons.pause.addEventListener('click', fnPause);
 buttons.prev.addEventListener('click', fnPrev);
